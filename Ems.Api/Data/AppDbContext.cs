2b1f158cfeff6db59f02624e4d9bdc2a7d27646d
eKonis-src <eu.khedr.nader@gmail.com>
2026-09-01 15:51:14 +0200

    Project init

---
+++ Ems.Api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Ems.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ems.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<People> Employees => Set<People>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<People>()
+            .HasIndex(e => e.Email)
+            .IsUnique();
+    }
+}
---
+++ Ems.Api/Models/People.cs
@@ -0,0 +1,12 @@
+namespace Ems.Api.Models;
+
+public class People
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string JobTitle { get; set; } = string.Empty;
+    public DateOnly HireDate { get; set; }
+    public bool IsActive { get; set; } = true;
+}
